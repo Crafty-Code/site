eaec6f82d173b59c68005f675226d4d27ee34aa5
Jeff <jeff.r.finn@gmail.com>
2025-06-30 07:04:48 +0100

    darkmode

--- themes/craftytheme/tailwind.config.js
+++ themes/craftytheme/tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './layouts/partials/**/*.html',
     './content/**/*.md',
     './assets/**/*.js',
+    './themes/craftytheme/layouts/**/*.html',
+    './themes/craftytheme/layouts/partials/**/*.html',
+    './themes/craftytheme/content/**/*.md',
+    './themes/craftytheme/assets/**/*.js',
   ],
   plugins: [
     require('@tailwindcss/typography'),
